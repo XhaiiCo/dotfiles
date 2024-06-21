dbb419fcc52470d2e37bc44601c9c5fe6c9e891b
Leclercq David <dle@bizzdev.com>
2024-06-21 09:38:23 +0200

    nvim colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,40 @@
+-- return {
+--         "folke/tokyonight.nvim",
+--         lazy = false,
+--         priority = 1000,
+--         opts = {},
+--         config = function()
+--                 vim.cmd("colorscheme tokyonight-storm")
+--         end
+-- }
+
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd("colorscheme tokyonight-storm")
-  end
+        "oxfist/night-owl.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+                -- load the colorscheme here
+                require("night-owl").setup()
+                vim.cmd("colorscheme night-owl")
+        end,
 }
 
 -- return {
---   -- catppuccin
---   {
---     "catppuccin/nvim",
---     tag = "v1.6.0",
---     lazy = false,
---     priority = 1000,
---     config = function()
---       local catppuccin = require("catppuccin")
+--         -- catppuccin
+--         {
+--                 "catppuccin/nvim",
+--                 tag = "v1.6.0",
+--                 lazy = false,
+--                 priority = 1000,
+--                 config = function()
+--                         local catppuccin = require("catppuccin")
 --
---       catppuccin.setup({
---         flavour = "mocha",
---         transparent_background = true,
---       })
+--                         catppuccin.setup({
+--                                 flavour = "mocha",
+--                                 transparent_background = true,
+--                         })
 --
---       vim.cmd("colorscheme catppuccin")
---     end,
---   }
+--                         vim.cmd("colorscheme catppuccin")
+--                 end,
+--         }
 -- }
