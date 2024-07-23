ae06bdf0df77c72505827af4d09275381df2e154
Leclercq David <dle@bizzdev.com>
2024-07-23 09:32:46 +0200

    Config neo tree with relative line number

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,35 +1,47 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  tag = "3.14",
-  lazy = false,
-  keys = {
-    { "<leader>e", "<cmd>Neotree toggle reveal position=float<cr>", desc = "Toggle Neotree" },
-  },
-  opts = {
-    filesystem = {
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = false,
-      },
-    },
-    -- With Y key, copy the path of the current file to the clipboard
-    window = {
-      mappings = {
-        ["<space>"] = "none",
-        ["Y"] = {
-          function(state)
-            local node = state.tree:get_node()
-            local path = node:get_id()
-            vim.fn.setreg("+", path, "c")
-          end,
-          desc = "copy path to clipboard",
+        "nvim-neo-tree/neo-tree.nvim",
+        tag = "3.14",
+        lazy = false,
+        keys = {
+                { "<leader>e", "<cmd>Neotree toggle reveal position=float<cr>", desc = "Toggle Neotree" },
         },
-      },
-    },
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
+        opts = {
+                filesystem = {
+                        follow_current_file = {
+                                enabled = true,
+                                leave_dirs_open = false,
+                        },
+                },
+                -- With Y key, copy the path of the current file to the clipboard
+                window = {
+                        mappings = {
+                                ["<space>"] = "none",
+                                ["Y"] = {
+                                        function(state)
+                                                local node = state.tree:get_node()
+                                                local path = node:get_id()
+                                                vim.fn.setreg("+", path, "c")
+                                        end,
+                                        desc = "copy path to clipboard",
+                                },
+                        },
+                },
+        },
+        dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-tree/nvim-web-devicons",
+                "MunifTanjim/nui.nvim",
+        },
+        config = function()
+                require("neo-tree").setup({
+                        event_handlers = {
+                                {
+                                        event = "neo_tree_buffer_enter",
+                                        handler = function()
+                                                vim.opt.relativenumber = true
+                                        end,
+                                }
+                        }
+                })
+        end,
 }
